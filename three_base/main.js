2d23bf0e9164c0fa5b51d60df6878086a65b6ab3
maa <maa0157@gmail.com>
2024-01-07 17:17:18 +0900

    球体作成中

--- three_base/main.js
+++ three_base/main.js
@@ -11,10 +11,20 @@ camera = new THREE.PerspectiveCamera(
   0.1, //開始距離
   1000 //終了距離
 );
+camera.position.set(0,0,500);
 
 //レンダーを追加
-renderer = new THREE.WebGLRenderer();
+renderer = new THREE.WebGLRenderer({alpha:true});
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+//ジオメトリを作成
+let ballGeometory = new THREE.SphereGeometry(100,64,32);
+//マテリアルを追加
+let ballMaterial = new THREE.MeshPhysicalMaterial();
+//メッシュを追加
+let ballMesh = new THREE.Mesh(ballGeometory,ballMaterial);
+scene.add(ballMesh);
+
 
 renderer.render(scene,camera);
