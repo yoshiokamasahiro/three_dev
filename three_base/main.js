65a31d3030669e298b92cbbf8219d2866412e601
maa <maa0157@gmail.com>
2024-01-09 13:43:09 +0900

    ポイント光源を球体の周りに周回させた

--- three_base/main.js
+++ three_base/main.js
@@ -1,6 +1,6 @@
 import * as THREE from "./node_modules/three/build/three.module.js";
 
-let scene, camera, renderer;
+let scene, camera, renderer, pointLight;
 
 //シーンを追加
 scene = new THREE.Scene();
@@ -31,5 +31,27 @@ let directionalLight = new THREE.DirectionalLight(0xffffff,2);
 directionalLight.position.set(1,1,1);
 scene.add(directionalLight);
 
+//ポイントライトを追加(グローバルに入れる)
+pointLight = new THREE.PointLight(0xffffff, 5000);
+// pointLight.position.set(200,200,-200);
+scene.add(pointLight);
 
-renderer.render(scene,camera);
+//ポイントライトがどこから入っているのか確認する
+let pointLightHelper = new THREE.PointLightHelper(pointLight,30);
+scene.add(pointLightHelper);
+
+//ポイント光源を球体周りで移動させる
+function animate() {
+  pointLight.position.set(
+    200 * Math.sin(Date.now() / 500),
+    200 * Math.sin(Date.now() / 1000),
+    200 * Math.cos(Date.now() / 500)
+  );
+  //フィレーム単位で動かす(Three.js)
+  requestAnimationFrame(animate);
+  //レンダリング
+  renderer.render(scene,camera);
+}
+
+animate();
+
