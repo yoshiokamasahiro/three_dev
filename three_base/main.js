9677cc2a49bda507d05aa97cbf1a273ba665faa0
maa <maa0157@gmail.com>
2024-01-10 19:46:59 +0900

    ボックスジオメトリとバッファジオメトリの作成(途中)

--- three_base/main.js
+++ three_base/main.js
@@ -15,7 +15,7 @@ window.addEventListener("load", ()=> {
     0.1, //開始距離
     1000 //終了距離
   );
-  camera.position.set(0,0,500);
+  camera.position.set(100,100,500);
 
   //レンダラーを追加(共通項的な理解でOKかと思う)
   renderer = new THREE.WebGLRenderer({alpha:true});
@@ -32,6 +32,50 @@ window.addEventListener("load", ()=> {
   //メッシュを追加
   let ballMesh = new THREE.Mesh(ballGeometory,ballMaterial);
   scene.add(ballMesh);
+
+  // ボックスジオメトリを追加
+  let cubeGeometry = new THREE.BoxGeometry(20,20,20);
+  let cubeMaterial = new THREE.MeshNormalMaterial();
+  let cube = new THREE.Mesh(cubeGeometry,cubeMaterial);
+  cube.position.set(100,100,100);
+  scene.add(cube);
+
+  // ▽▽ここからバッファジオメトリ▽▽
+  //バッファジオメトリを追加
+  const bufferGeometory = new THREE.BufferGeometry();
+  //型付配列で位置情報を作成
+  const positionArray = new Float32Array(9);
+  //9つ準備
+  positionArray[0] = 0;
+  positionArray[1] = 0;
+  positionArray[2] = 0;
+
+  positionArray[3] = 0;
+  positionArray[4] = 100;
+  positionArray[5] = 0;
+
+  positionArray[6] = 100;
+  positionArray[7] = 0;
+  positionArray[8] = 0;
+  // 整数の3は三角形の1つの頂点に対して3つの値が必要なため
+  const bufferAttribute = new THREE.BufferAttribute(positionArray,3);
+  //上の工程を経てジオメトリー作成完了
+  bufferGeometory.setAttribute('position',bufferAttribute);
+  //マテリアル指定
+  const bufferMaterial = new THREE.MeshBasicMaterial(
+    {wireframe:true,} //見にくいのでワイヤーフレーム化
+  );
+
+  const buffer = new THREE.Mesh(bufferGeometory,bufferMaterial);
+  buffer.position.set(-100,-100,-100);
+  scene.add(buffer);
+  // △△ここまでバッファジオメトリ△△
+
+  //アンビエントライトを追加
+  // let anbientLight = new THREE.AmbientLight(0xffffff,0.8);
+  // anbientLight.position.set(1,1,1);
+  // scene.add(anbientLight);
+
 
   //並行光源を追加
   let directionalLight = new THREE.DirectionalLight(0xffffff,2);
